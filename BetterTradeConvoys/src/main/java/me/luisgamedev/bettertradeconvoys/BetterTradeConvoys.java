cac832d0ade41c1d4abab8f588628859cc3e9708
Luis-Dev <luis.ljate@gmail.com>
2025-09-03 16:02:18 +0200

    Remove convoy commands and add start announcement option

--- BetterTradeConvoys/src/main/java/me/luisgamedev/bettertradeconvoys/BetterTradeConvoys.java
+++ BetterTradeConvoys/src/main/java/me/luisgamedev/bettertradeconvoys/BetterTradeConvoys.java
@@ -4,11 +4,9 @@ import me.luisgamedev.bettertradeconvoys.language.LanguageManager;
 import me.luisgamedev.bettertradeconvoys.listeners.CitizensListeners;
 import me.luisgamedev.bettertradeconvoys.listeners.DepositListener;
 import me.luisgamedev.bettertradeconvoys.listeners.RoutesGuiListener;
-import me.luisgamedev.bettertradeconvoys.service.ClaimStore;
 import me.luisgamedev.bettertradeconvoys.service.ConvoyManager;
 import me.luisgamedev.bettertradeconvoys.service.PlayerProgressStore;
 import me.luisgamedev.bettertradeconvoys.service.RoutesConfig;
-import me.luisgamedev.bettertradeconvoys.commands.ConvoyCommand; // falls dein Command woanders liegt, passe das an
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,7 +17,6 @@ public final class BetterTradeConvoys extends JavaPlugin {
     private LanguageManager language;
     private RoutesConfig routesConfig;
     private PlayerProgressStore progressStore;
-    private ClaimStore claimStore;
     private ConvoyManager convoyManager;
 
     @Override
@@ -33,26 +30,16 @@ public final class BetterTradeConvoys extends JavaPlugin {
 
         // Manager/Stores laden
         language = new LanguageManager(this);
+        language.load();
         routesConfig = new RoutesConfig(this); routesConfig.load();
         progressStore = new PlayerProgressStore(this); progressStore.load();
-        claimStore = new ClaimStore(this);
-        try {
-            ClaimStore.class.getMethod("load");
-            claimStore.load();
-        } catch (NoSuchMethodException ignored) {}
 
-        convoyManager = new ConvoyManager(this, routesConfig, progressStore, claimStore, language);
+        convoyManager = new ConvoyManager(this, routesConfig, progressStore, language);
         convoyManager.initCitizensCheck();
 
         Bukkit.getPluginManager().registerEvents(new CitizensListeners(convoyManager, language, routesConfig), this);
         Bukkit.getPluginManager().registerEvents(new DepositListener(convoyManager), this);
         Bukkit.getPluginManager().registerEvents(new RoutesGuiListener(convoyManager, language), this);
-
-        if (getCommand("convoy") != null) {
-            getCommand("convoy").setExecutor(new ConvoyCommand(this, convoyManager, routesConfig, claimStore, language));
-        } else {
-            getLogger().warning("Command 'convoy' not found in plugin.yml – please add it!");
-        }
 
         getLogger().info("BetterTradeConvoys enabled.");
     }
@@ -66,13 +53,6 @@ public final class BetterTradeConvoys extends JavaPlugin {
         if (progressStore != null) {
             progressStore.save();
         }
-        if (claimStore != null) {
-            try {
-                ClaimStore.class.getMethod("save");
-                claimStore.save();
-            } catch (NoSuchMethodException ignored) {}
-        }
-
         getLogger().info("BetterTradeConvoys disabled.");
     }
 
@@ -93,10 +73,6 @@ public final class BetterTradeConvoys extends JavaPlugin {
 
     public PlayerProgressStore progress() {
         return progressStore;
-    }
-
-    public ClaimStore claims() {
-        return claimStore;
     }
 
     public ConvoyManager convoys() {
